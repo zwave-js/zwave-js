154e9e700b278a58b3c5f354e37588c9ce157486
Dominic Griesel <dominic.griesel@nabucasa.com>
2025-04-02 22:00:14 +0200

    feat: query channel info from radio and use for encoding

--- packages/serial/src/rcp/capability/GetFirmwareInfoMessages.ts
+++ packages/serial/src/rcp/capability/GetFirmwareInfoMessages.ts
@@ -11,14 +11,19 @@ import {
 	rcpMessageTypes,
 } from "../../message/RCPMessages.js";
 
+export enum RadioLibrary {
+	RAIL,
+}
+
 @rcpMessageTypes(RCPMessageType.Request, RCPFunctionType.GetFirmwareInfo)
 // @priority(MessagePriority.Normal)
 @expectedRcpResponse(RCPFunctionType.GetFirmwareInfo)
 export class GetFirmwareInfoRequest extends RCPMessage {}
 
 export interface GetFirmwareInfoResponseOptions {
-	firmwareType: number;
-	firmwareVersion: string;
+	rcpFirmwareVersion: string;
+	radioLibrary: RadioLibrary;
+	radioLibraryVersion: string;
 	supportedFunctionTypes: RCPFunctionType[];
 }
 
@@ -28,8 +33,9 @@ export class GetFirmwareInfoResponse extends RCPMessage {
 		options: GetFirmwareInfoResponseOptions & RCPMessageBaseOptions,
 	) {
 		super(options);
-		this.firmwareType = options.firmwareType;
-		this.firmwareVersion = options.firmwareVersion;
+		this.rcpFirmwareVersion = options.rcpFirmwareVersion;
+		this.radioLibrary = options.radioLibrary;
+		this.radioLibraryVersion = options.radioLibraryVersion;
 		this.supportedFunctionTypes = options.supportedFunctionTypes;
 	}
 
@@ -37,38 +43,53 @@ export class GetFirmwareInfoResponse extends RCPMessage {
 		raw: RCPMessageRaw,
 		_ctx: RCPMessageParsingContext,
 	): GetFirmwareInfoResponse {
-		const firmwareType = raw.payload[0];
-		const majorVersion = raw.payload[1];
-		const minorVersion = raw.payload[2];
-		const patchVersion = raw.payload[3];
-		const firmwareVersion =
+		let offset = 0;
+		const majorVersion = raw.payload[offset++];
+		const minorVersion = raw.payload[offset++];
+		const patchVersion = raw.payload[offset++];
+		const rcpFirmwareVersion =
 			`${majorVersion}.${minorVersion}.${patchVersion}`;
 
-		const functionTypeBitmaskLength = raw.payload[4];
+		const radioLibrary = raw.payload[offset++];
+
+		const radioMajorVersion = raw.payload[offset++];
+		const radioMinorVersion = raw.payload[offset++];
+		const radioPatchVersion = raw.payload[offset++];
+		const radioLibraryVersion =
+			`${radioMajorVersion}.${radioMinorVersion}.${radioPatchVersion}`;
+
+		const functionTypeBitmaskLength = raw.payload[offset++];
 		const supportedFunctionTypes: RCPFunctionType[] = parseBitMask(
-			raw.payload.slice(5, 5 + functionTypeBitmaskLength),
+			raw.payload.slice(offset, offset + functionTypeBitmaskLength),
 		);
 
 		return new this({
-			firmwareType,
-			firmwareVersion,
+			rcpFirmwareVersion,
+			radioLibrary,
+			radioLibraryVersion,
 			supportedFunctionTypes,
 		});
 	}
 
-	public firmwareType: number;
-	public firmwareVersion: string;
+	public rcpFirmwareVersion: string;
+	public radioLibrary: RadioLibrary;
+	public radioLibraryVersion: string;
 	public supportedFunctionTypes: RCPFunctionType[];
 
 	public serialize(ctx: RCPMessageEncodingContext): Promise<Bytes> {
-		const firmwareBytes = this.firmwareVersion
+		const rcpVersionBytes = this.rcpFirmwareVersion
 			.split(".", 2)
 			.map((str) => parseInt(str));
+		const radioVersionBytes = this.radioLibraryVersion
+			.split(".", 2)
+			.map((str) => parseInt(str));
+
 		const functionTypeBitmask = encodeBitMask(this.supportedFunctionTypes);
 
 		this.payload = Bytes.concat([
-			[1], // firmware type RCP
-			firmwareBytes,
+			rcpVersionBytes,
+			[this.radioLibrary],
+			radioVersionBytes,
 			[functionTypeBitmask.length],
 			functionTypeBitmask,
 		]);
