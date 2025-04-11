8fbfecd55d108f9991d937491c35e134dcaef980
Dominic Griesel <dominic.griesel@nabucasa.com>
2025-04-11 09:38:52 +0200

    revert: changes to test script

--- test/run.ts
+++ test/run.ts
@@ -1,10 +1,10 @@
 import { wait as _wait } from "alcalzone-shared/async";
 import path from "node:path";
 import "reflect-metadata";
-import { Bytes, getEnumMemberName } from "@zwave-js/shared/safe";
+import { Bytes } from "@zwave-js/shared/safe";
 import _os from "node:os";
 import { fileURLToPath } from "node:url";
-import { Driver, NabuCasaCommand, type RGB, type Vector } from "zwave-js";
+import { Driver } from "zwave-js";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
@@ -15,11 +15,17 @@ process.on("unhandledRejection", (_r) => {
 	debugger;
 });
 
-const port = "/dev/ttyACM0";
-
-const red: RGB = { r: 255, g: 0, b: 0 };
-const yellow: RGB = { r: 200, g: 140, b: 0 };
-const green: RGB = { r: 0, g: 255, b: 0 };
+// const port = "tcp://Z-Net-R2v2.local:2001";
+// 500/700 series
+// const port = require("node:os").platform() === "win32"
+// 	? "COM5"
+// 	: "/dev/ttyACM0";
+// const port = require("os").platform() === "win32" ? "COM5" : "/dev/ttyUSB0";
+// 800 series
+const port = os.platform() === "win32"
+	? "COM5"
+	: "/dev/serial/by-id/usb-Zooz_800_Z-Wave_Stick_533D004242-if00";
+// const port = "tcp://127.0.0.1:5555";
 
 const driver = new Driver(port, {
 	// logConfig: {
@@ -67,32 +73,6 @@ const driver = new Driver(port, {
 	.on("error", console.error)
 	.once("driver ready", async () => {
 		// Test code goes here
-		const nc = driver.controller.proprietary["Nabu Casa"];
-		if (!nc) return;
-
-		const supportedCommands = await nc.getSupportedCommands();
-		console.log(
-			`supported commands: ${
-				supportedCommands.map((c) =>
-					getEnumMemberName(NabuCasaCommand, c)
-				).join(", ")
-			}`,
-		);
-
-		for (let i = 0; i < 20; i++) {
-			await wait(500);
-			const gyro = await nc.readGyro();
-			const angle = vectorAngle(gyro!, { x: 0, y: 0, z: -1 });
-			console.log("angle from vertical: ", angle);
-
-			if (angle < 20) {
-				await nc.setLED(green);
-			} else if (angle < 45) {
-				await nc.setLED(yellow);
-			} else {
-				await nc.setLED(red);
-			}
-		}
 	})
 	.once("bootloader ready", async () => {
 		// What to do when stuck in the bootloader
@@ -106,11 +86,3 @@ void driver.start();
 process.on("SIGINT", async () => {
 	await driver.destroy();
 });
-
-function vectorAngle(v1: Vector, v2: Vector): number {
-	const dotProduct = v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
-	const magnitudeV1 = Math.hypot(v1.x, v1.y, v1.z);
-	const magnitudeV2 = Math.hypot(v2.x, v2.y, v2.z);
-	const cosTheta = dotProduct / (magnitudeV1 * magnitudeV2);
-	return Math.acos(cosTheta) * (180 / Math.PI); // Winkel in Grad
-}
