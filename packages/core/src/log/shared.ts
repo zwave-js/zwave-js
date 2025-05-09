e5ab466e0b20143463d61fac19c94d6193f43a23
Dominic Griesel <dominic.griesel@nabucasa.com>
2025-05-09 14:03:18 +0200

    fix: use local time for logging to file

--- packages/core/src/log/shared.ts
+++ packages/core/src/log/shared.ts
@@ -6,7 +6,8 @@ export const timestampFormatShort = "HH:mm:ss.SSS";
 export const timestampPaddingShort = " ".repeat(
 	timestampFormatShort.length + 1,
 );
-export const timestampPadding = " ".repeat(new Date().toISOString().length + 1);
+export const timestampFormat = "YYYY-MM-DD HH:mm:ss.SSS";
+export const timestampPadding = " ".repeat(timestampFormat.length + 1);
 /** @internal */
 export const channelPadding = " ".repeat(7); // 6 chars channel name, 1 space
 
