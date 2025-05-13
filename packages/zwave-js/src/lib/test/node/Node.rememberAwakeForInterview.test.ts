d6e41a9cf236d62af03de1437c53e6a62fe0377d
Dominic Griesel <dominic.griesel@nabucasa.com>
2025-05-13 14:23:08 +0200

    test: consider both waitForWakeup = true and false

--- packages/zwave-js/src/lib/test/node/Node.rememberAwakeForInterview.test.ts
+++ packages/zwave-js/src/lib/test/node/Node.rememberAwakeForInterview.test.ts
@@ -5,7 +5,7 @@ import path from "node:path";
 import { integrationTest } from "../integrationTestSuite.js";
 
 integrationTest(
-	"If a node is considered awake when the interview starts, this should be remembered",
+	"If a node is considered awake when the interview starts, this should be remembered (waitForWakeup = true)",
 	{
 		// debug: true,
 		provisioningDirectory: path.join(__dirname, "fixtures/sleepingNode"),
@@ -44,7 +44,7 @@ integrationTest(
 );
 
 integrationTest(
-	"If the interview waits for a node to wake up, it should be remembered as being awake",
+	"If the interview waits for a node to wake up, it should be remembered as being awake (waitForWakeup = true)",
 	{
 		// debug: true,
 		provisioningDirectory: path.join(__dirname, "fixtures/sleepingNode"),
@@ -66,6 +66,87 @@ integrationTest(
 			// Mark the node as awake shortly after the interview starts
 			node.markAsAsleep();
 			void node.refreshInfo();
+			setTimeout(() => {
+				node.markAsAwake();
+			}, 100);
+
+			const interviewDone = createDeferredPromise<void>();
+			node.once("interview completed", () => {
+				interviewDone.resolve();
+			});
+
+			// The interview should complete quickly
+			const testResult = await Promise.race([
+				interviewDone.then(() => true),
+				wait(2000).then(() => false),
+			]);
+			t.expect(testResult, "The interview did not complete").toBe(true);
+		},
+	},
+);
+
+integrationTest(
+	"If a node is considered awake when the interview starts, this should be remembered (waitForWakeup = false)",
+	{
+		// debug: true,
+		provisioningDirectory: path.join(__dirname, "fixtures/sleepingNode"),
+
+		nodeCapabilities: {
+			manufacturerId: 0x0000,
+			productType: 0xdead,
+			productId: 0xbeef,
+
+			isListening: false,
+			isFrequentListening: false,
+
+			commandClasses: [
+				CommandClasses["Manufacturer Specific"],
+			],
+		},
+
+		testBody: async (t, driver, node, mockController, mockNode) => {
+			// Mark the node as awake shortly before the interview starts
+			node.markAsAwake();
+			void node.refreshInfo({ waitForWakeup: false });
+
+			const interviewDone = createDeferredPromise<void>();
+			node.once("interview completed", () => {
+				interviewDone.resolve();
+			});
+
+			// The interview should complete quickly
+			const testResult = await Promise.race([
+				interviewDone.then(() => true),
+				wait(2000).then(() => false),
+			]);
+			t.expect(testResult, "The interview did not complete").toBe(true);
+		},
+	},
+);
+
+integrationTest(
+	"If the interview waits for a node to wake up, it should be remembered as being awake (waitForWakeup = false)",
+	{
+		// debug: true,
+		provisioningDirectory: path.join(__dirname, "fixtures/sleepingNode"),
+
+		nodeCapabilities: {
+			manufacturerId: 0x0000,
+			productType: 0xdead,
+			productId: 0xbeef,
+
+			isListening: false,
+			isFrequentListening: false,
+
+			commandClasses: [
+				CommandClasses["Manufacturer Specific"],
+			],
+		},
+
+		testBody: async (t, driver, node, mockController, mockNode) => {
+			// Mark the node as awake shortly after the interview starts
+			node.markAsAsleep();
+			void node.refreshInfo({ waitForWakeup: false });
 			setTimeout(() => {
 				node.markAsAwake();
 			}, 100);
