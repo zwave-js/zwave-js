275dc6cb80a2c284a383a54bb9ae1169bb0a780a
Dominic Griesel <dominic.griesel@nabucasa.com>
2025-06-17 16:54:35 +0200

    fix: support parsing partial command and data frames

--- packages/zwave-js/src/lib/zniffer/Zniffer.ts
+++ packages/zwave-js/src/lib/zniffer/Zniffer.ts
@@ -54,7 +54,6 @@ import {
 	ZnifferGetVersionRequest,
 	ZnifferGetVersionResponse,
 	ZnifferMessage,
-	ZnifferMessageRaw,
 	ZnifferMessageType,
 	ZnifferSerialFrameType,
 	type ZnifferSerialStream,
@@ -1116,29 +1115,52 @@ supported frequencies: ${
 		let { bytesRead: offset } = parseZLFHeader(buffer);
 
 		this.clearCapturedFrames();
+		let accumulator: CapturedData | undefined;
 
 		while (offset < buffer.length) {
-			const { msg, entry, bytesRead } = parseZLFEntry(buffer, offset);
+			const {
+				bytesRead,
+				complete,
+				type,
+				entry,
+				msg,
+				accumulator: newAccumulator,
+			} = parseZLFEntry(
+				buffer,
+				offset,
+				accumulator,
+			);
 			// Avoid infinite loops
 			if (bytesRead <= 0) break;
+			offset += bytesRead;
 
-			if (entry) {
-				if (msg) {
-					entry.parsedFrame = (await this.parseFrame(msg))?.external;
-				}
+			if (complete) {
+				accumulator = undefined;
+			} else {
+				accumulator = newAccumulator;
+				continue;
+			}
+
+			if (type === ZnifferMessageType.Data) {
+				entry.parsedFrame =
+					// FIXME: Figure out which values the Zniffer application actually stores
+					// as RSSI. Both the attempted conversion and the raw values seem wrong.
+					(await this.parseFrame(msg, false)).external;
 				this._capturedFrames.push(entry);
 			}
-			offset += bytesRead;
 		}
 	}
 
-	private async parseFrame(msg: ZnifferDataMessage): Promise<{
+	private async parseFrame(
+		msg: ZnifferDataMessage,
+		convertRSSI: boolean = this._options.convertRSSI ?? false,
+	): Promise<{
 		internal: any;
 		cc?: CommandClass;
 		external: Frame | CorruptedFrame;
 	}> {
 		let convertedRSSI: RSSI | undefined;
-		if (this._options.convertRSSI && this._chipType) {
+		if (convertRSSI && this._chipType) {
 			convertedRSSI = tryConvertRSSI(
 				msg.rssiRaw,
 				this._chipType,
@@ -1365,17 +1387,54 @@ function parseZLFHeader(buffer: Uint8Array): {
 	};
 }
 
-function parseZLFEntry(buffer: Uint8Array, offset: number): {
-	msg: ZnifferDataMessage | undefined;
-	entry: CapturedData | undefined;
-	bytesRead: number;
-} {
+type ParseZLFEntryResult =
+	| (
+		& {
+			complete: true;
+			bytesRead: number;
+			accumulator?: undefined;
+		}
+		& (
+			| {
+				type: ZnifferMessageType.Data;
+				msg: ZnifferDataMessage;
+				entry: CapturedData;
+			}
+			| {
+				type: ZnifferMessageType.Command;
+				msg: ZnifferMessage;
+				entry: CapturedData;
+			}
+			| {
+				type?: undefined;
+				msg?: undefined;
+				entry?: undefined;
+			}
+		)
+	)
+	| ({
+		complete: false;
+		bytesRead: number;
+		accumulator: CapturedData;
+		type?: undefined;
+		msg?: undefined;
+		entry?: undefined;
+	});
+
+/** @internal */
+export function parseZLFEntry(
+	buffer: Uint8Array,
+	offset: number,
+	accumulator?: CapturedData,
+): ParseZLFEntryResult {
 	const bytes = Bytes.view(buffer.subarray(offset));
 
-	// Each ZLF entry has a 14-byte overhead
+	// Each ZLF entry has a 14-byte overhead, so the buffer must have at least that size
 	if (bytes.length < 14) {
-		// Incomplete entry
-		return { msg: undefined, entry: undefined, bytesRead: 0 };
+		throw new ZWaveError(
+			"Invalid ZLF file: entry truncated",
+			ZWaveErrorCodes.PacketFormat_Truncated,
+		);
 	}
 
 	// Parse .NET DateTime ticks (8 bytes, little-endian)
@@ -1392,55 +1451,78 @@ function parseZLFEntry(buffer: Uint8Array, offset: number): {
 	// Ignore the direction and session ID byte
 	const rawDataLength = bytes[9];
 	const totalLength = 14 + rawDataLength;
-	if (
-		bytes.length < totalLength
-		// The entry should end with EOF marker 0xfe
-		|| bytes[totalLength - 1] !== 0xfe
-	) {
-		// Incomplete or invalid entry
-		return { msg: undefined, entry: undefined, bytesRead: 0 };
+	if (bytes.length < totalLength) {
+		throw new ZWaveError(
+			"Invalid ZLF file: entry truncated",
+			ZWaveErrorCodes.PacketFormat_Truncated,
+		);
+	}
+	const eod = bytes[totalLength - 1];
+	// 0xfe designates a Zniffer entry
+	if (eod !== 0xfe) {
+		// Skip unsupported entries
+		return {
+			complete: true,
+			bytesRead: totalLength,
+		};
 	}
 	// bytes 10-12 are empty
 
-	const rawData = bytes.subarray(13, totalLength - 1);
-
-	// The encoding in the file is a bit weird
-	// - Command frames start with the SOCF (0x23) byte
-	// - Data frames DO NOT start with the SODF (0x21) byte, but with the first byte of the data
-	// e.g.:
-	// 	230500  -> command
-	// 	23      -> command
-	// 	010000020b2c21030cc4dae6070141010c02009177 -> data
-
-	if (rawData[0] === ZnifferMessageType.Command) {
-		// We're not interested in command frames here
-		return { msg: undefined, entry: undefined, bytesRead: totalLength };
+	let rawData = bytes.subarray(13, totalLength - 1);
+	if (accumulator) {
+		rawData = Bytes.concat([
+			accumulator.rawData,
+			rawData,
+		]);
 	}
 
-	const raw = new ZnifferMessageRaw(
-		ZnifferMessageType.Data,
-		undefined,
-		rawData,
-	);
-
-	let msg: ZnifferDataMessage;
 	try {
-		msg = ZnifferDataMessage.from(raw);
-	} catch {
-		// Discard entries where parsing fails
-		return { msg: undefined, entry: undefined, bytesRead: totalLength };
+		const msg = ZnifferMessage.parse(rawData);
+
+		const entry: CapturedData = {
+			timestamp,
+			rawData,
+			frameData: msg.payload,
+		};
+
+		// Help TypeScript out a bit
+		if (msg instanceof ZnifferDataMessage) {
+			return {
+				complete: true,
+				bytesRead: totalLength,
+				type: ZnifferMessageType.Data,
+				msg,
+				entry,
+			};
+		} else {
+			// We are dealing with a command frame
+			return {
+				complete: true,
+				bytesRead: totalLength,
+				type: ZnifferMessageType.Command,
+				msg,
+				entry,
+			};
+		}
+	} catch (e) {
+		if (
+			isZWaveError(e) && e.code === ZWaveErrorCodes.PacketFormat_Truncated
+		) {
+			// We are dealing with an incomplete frame, so we need to accumulate the data for the next iteration
+			accumulator ??= {
+				timestamp,
+				rawData: new Bytes(),
+				frameData: new Bytes(), // Cannot be determined yet
+			};
+			accumulator.rawData = rawData; // rawData is already concatenated
+
+			return {
+				complete: false,
+				bytesRead: totalLength,
+				accumulator,
+			};
+		}
+
+		throw e;
 	}
-
-	// Discard entries whose type is not ZnifferMessageType.Data
-	if (msg.type !== ZnifferMessageType.Data) {
-		return { msg: undefined, entry: undefined, bytesRead: totalLength };
-	}
-
-	const entry: CapturedData = {
-		timestamp,
-		rawData,
-		frameData: msg.payload,
-	};
-
-	return { msg, entry, bytesRead: totalLength };
 }
