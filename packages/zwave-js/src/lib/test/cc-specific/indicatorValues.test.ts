3956f6deb042e57e1c9f45c2bfd46306d61e9b48
Dominic Griesel <dominic.griesel@nabucasa.com>
2025-07-15 13:24:07 +0200

    test: create test for exposed Indicator CC values

---
+++ packages/zwave-js/src/lib/test/cc-specific/indicatorValues.test.ts
@@ -0,0 +1,75 @@
+import { CommandClasses, Indicator } from "@zwave-js/core";
+import { pick } from "@zwave-js/shared";
+import { integrationTest } from "../integrationTestSuite.js";
+
+integrationTest(
+	"Indicator values exposed to the user are human-friendly and have sensible labels",
+	{
+		// debug: true,
+
+		nodeCapabilities: {
+			commandClasses: [
+				CommandClasses.Version,
+				{
+					ccId: CommandClasses.Indicator,
+					version: 4,
+					indicators: {
+						[Indicator["Button 1 indication"]]: {
+							properties: [
+								0x02, // Binary
+								0x03, // On/Off Periods
+								0x04, // On/Off Cycle count
+								0x05, // On/Off Period: On time
+							],
+						},
+						[Indicator["Button 2 indication"]]: {
+							properties: [
+								0x01, // Multilevel
+								0x0A, // Timeout hours
+							],
+						},
+						[Indicator["Button 3 indication"]]: {
+							properties: [
+								0x02, // Binary
+								0x06, // Timeout minutes
+							],
+						},
+					},
+				},
+			],
+		},
+
+		testBody: async (t, driver, node, mockController, mockNode) => {
+			const indicatorValueIds = node.getDefinedValueIDs()
+				.filter((vid) => vid.commandClass === CommandClasses.Indicator)
+				// Ignore the "identify" value
+				.filter((vid) => vid.property !== "identify")
+				// Ignore the legacy v1 value
+				.filter((vid) => vid.property !== "value");
+
+			// There should only be 3 binary/multilevel values
+			// t.expect(indicatorValueIds.length).toBe(3);
+			const humanReadable = indicatorValueIds.map((vid) => {
+				return pick(vid, [
+					"propertyName",
+					"propertyKeyName",
+				]);
+			});
+
+			t.expect(humanReadable).toEqual([
+				{
+					propertyName: "Button 1 indication",
+					propertyKeyName: "Binary",
+				},
+				{
+					propertyName: "Button 2 indication",
+					propertyKeyName: "Multilevel",
+				},
+				{
+					propertyName: "Button 3 indication",
+					propertyKeyName: "Binary",
+				},
+			]);
+		},
+	},
+);
