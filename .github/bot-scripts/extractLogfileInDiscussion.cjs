9bc580264041157e7a7a6526ff48c1ed8cc4b8f7
AlCalzone <dominic.griesel@nabucasa.com>
2025-08-14 14:52:51 +0200

    ci: automatically analyze Z-Wave JS logfiles in discussions (#8070)

--- .github/bot-scripts/extractLogfileInDiscussion.cjs
+++ .github/bot-scripts/extractLogfileInDiscussion.cjs
@@ -2,8 +2,7 @@
 
 /// <reference path="types.d.ts" />
 
-const markdownLinkRegex = /\[.*\]\((http.*?)\)/;
-const codeBlockRegex = /`{3,4}(.*?)`{3,4}/s;
+const { extractLogfileSection, extractLogfileContent } = require("./utils.cjs");
 
 /**
  * @param {{github: Github, context: Context}} param
@@ -24,49 +23,17 @@ async function main(param) {
 
 	const logfileSectionHeader = "### Upload Logfile";
 
-	console.log("secitonHeader found:", body.includes(logfileSectionHeader));
+	console.log("sectionHeader found:", body.includes(logfileSectionHeader));
 
 	// Check if this is a bug report which requires a logfile
 	if (!body.includes(logfileSectionHeader)) return;
 
-	const logfileSection = body.slice(
-		body.indexOf(logfileSectionHeader) + logfileSectionHeader.length,
-	);
-
-	console.log("logfileSection:", logfileSection);
-
-	const link = markdownLinkRegex.exec(logfileSection)?.[1]?.trim();
-	const codeBlockContent = codeBlockRegex.exec(logfileSection)?.[1]?.trim();
-
-	console.log("link:", link);
-	console.log("codeBlockContent:", codeBlockContent);
-
-	if (link) {
-		let logFile;
-		try {
-			const resp = await fetch(link);
-			if (!resp.ok) {
-				console.error(
-					`Failed to fetch logfile from ${link}:`,
-					resp.statusText,
-				);
-				return "ERROR_FETCH";
-			}
-			logFile = await resp.text();
-		} catch (e) {
-			console.error(`Failed to fetch logfile from ${link}:`, e);
-			return "ERROR_FETCH";
-		}
-
-		// limit to the last 250 lines
-		return logFile.split("\n").slice(-250).join("\n");
-	} else if (codeBlockContent) {
-		if (codeBlockContent.split("\n").length > 20) {
-			// This code block is too long and should be a logfile instead
-			return "ERROR_CODE_BLOCK_TOO_LONG";
-		}
-
-		return codeBlockContent;
+	try {
+		const logfileSection = extractLogfileSection(body);
+		return await extractLogfileContent(logfileSection);
+	} catch (error) {
+		console.error("Error extracting logfile:", error);
+		return null;
 	}
 }
 
