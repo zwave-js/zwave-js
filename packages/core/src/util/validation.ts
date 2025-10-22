888a34f02efce0255b658a6417c049fda49475d2
Dominic Griesel <dominic.griesel@nabucasa.com>
2025-10-22 15:45:38 +0200

    fix: validation error message for Uint8Array

--- packages/core/src/util/validation.ts
+++ packages/core/src/util/validation.ts
@@ -162,7 +162,7 @@ function formatElaboration(e: ErrorElaboration, indent: number = 0): string {
 				`Expected ${what} to be of type ${e.objectType}, got ${e.actual}`;
 		}
 	} else if (e.type === "uint8array") {
-		ret += `Expected ${what} to be a BytesView, got ${e.actual}`;
+		ret += `Expected ${what} to be a Uint8Array or BytesView, got ${e.actual}`;
 	} else if (e.type === "missing") {
 		ret += `ERROR: Missing validation for ${what}`;
 	} else if (e.type === "class") {
