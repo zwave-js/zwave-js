354037eb1faf74e8be217e79645b0df788ab6b10
Dominic Griesel <dominic.griesel@nabucasa.com>
2025-11-14 12:51:15 +0100

    test: simplify other test

--- packages/zwave-js/src/lib/test/driver/ignoreCCVersion0ForKnownSupportedCCs.test.ts
+++ packages/zwave-js/src/lib/test/driver/ignoreCCVersion0ForKnownSupportedCCs.test.ts
@@ -44,125 +44,22 @@ integrationTest(
 				CommandClasses.Version,
 				CommandClasses["Security 2"],
 			],
+			securityClasses: new Set([SecurityClass.S2_Unauthenticated]),
 		},
 
 		customSetup: async (driver, controller, mockNode) => {
-			// Create a security manager for the node
-			const smNode = await SecurityManager2.create();
-			// Copy keys from the driver
-			await smNode.setKey(
-				SecurityClass.S2_Unauthenticated,
-				driver.options.securityKeys!.S2_Unauthenticated!,
-			);
-			mockNode.securityManagers.securityManager2 = smNode;
-			mockNode.encodingContext.getHighestSecurityClass = () =>
-				SecurityClass.S2_Unauthenticated;
-
-			// Create a security manager for the controller
-			const smCtrlr = await SecurityManager2.create();
-			// Copy keys from the driver
-			await smCtrlr.setKey(
-				SecurityClass.S2_Unauthenticated,
-				driver.options.securityKeys!.S2_Unauthenticated!,
-			);
-			controller.securityManagers.securityManager2 = smCtrlr;
-			controller.encodingContext.getHighestSecurityClass = () =>
-				SecurityClass.S2_Unauthenticated;
-
-			// Respond to Nonce Get
-			const respondToNonceGet: MockNodeBehavior = {
-				async handleCC(controller, self, receivedCC) {
-					if (receivedCC instanceof Security2CCNonceGet) {
-						const nonce = await smNode.generateNonce(
-							controller.ownNodeId,
-						);
-						const cc = new Security2CCNonceReport({
-							nodeId: controller.ownNodeId,
-							SOS: true,
-							MOS: false,
-							receiverEI: nonce,
-						});
-						return { action: "sendCC", cc };
-					}
-				},
-			};
-			mockNode.defineBehavior(respondToNonceGet);
-
-			// Handle decode errors
-			const handleInvalidCC: MockNodeBehavior = {
-				async handleCC(controller, self, receivedCC) {
-					if (receivedCC instanceof InvalidCC) {
-						if (
-							receivedCC.reason
-								=== ZWaveErrorCodes.Security2CC_CannotDecode
-							|| receivedCC.reason
-								=== ZWaveErrorCodes.Security2CC_NoSPAN
-						) {
-							const nonce = await smNode.generateNonce(
-								controller.ownNodeId,
-							);
-							const cc = new Security2CCNonceReport({
-								nodeId: controller.ownNodeId,
-								SOS: true,
-								MOS: false,
-								receiverEI: nonce,
-							});
-							return { action: "sendCC", cc };
-						}
-					}
-				},
-			};
-			mockNode.defineBehavior(handleInvalidCC);
-
-			const reportSecurelySupportedCCs: MockNodeBehavior = {
-				handleCC(controller, self, receivedCC) {
-					if (
-						receivedCC
-							instanceof Security2CCMessageEncapsulation
-						&& receivedCC.securityClass
-							=== SecurityClass.S2_Unauthenticated
-						&& receivedCC.encapsulated
-							instanceof Security2CCCommandsSupportedGet
-					) {
-						let cc: CommandClass =
-							new Security2CCCommandsSupportedReport({
-								nodeId: controller.ownNodeId,
-								supportedCCs: [
-									// The node supports Version CC securely
-									CommandClasses.Version,
-								],
-							});
-						cc = Security2CC.encapsulate(
-							cc,
-							self.id,
-							self.securityManagers,
-						);
-						return { action: "sendCC", cc };
-					}
-				},
-			};
-			mockNode.defineBehavior(reportSecurelySupportedCCs);
-
 			const respondWithInvalidVersionReport: MockNodeBehavior = {
 				handleCC(controller, self, receivedCC) {
 					if (
-						receivedCC
-							instanceof Security2CCMessageEncapsulation
-						&& receivedCC.encapsulated
-							instanceof VersionCCCommandClassGet
-						&& receivedCC.encapsulated.requestedCC
+						receivedCC instanceof VersionCCCommandClassGet
+						&& receivedCC.requestedCC
 							=== CommandClasses["Security 2"]
 					) {
 						let cc: CommandClass = new VersionCCCommandClassReport({
 							nodeId: controller.ownNodeId,
-							requestedCC: receivedCC.encapsulated.requestedCC,
+							requestedCC: receivedCC.requestedCC,
 							ccVersion: 0,
 						});
-						cc = Security2CC.encapsulate(
-							cc,
-							self.id,
-							self.securityManagers,
-						);
 						return { action: "sendCC", cc };
 					}
 				},
