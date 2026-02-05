d17ce70d529dab711033b6931e0146a893fbc4f9
Dominic Griesel <dominic.griesel@nabucasa.com>
2026-02-05 15:14:36 +0100

    feat: define a semantic config parameter kind

--- packages/config/src/devices/ParamInformation.ts
+++ packages/config/src/devices/ParamInformation.ts
@@ -221,6 +221,18 @@ Parameter #${parameterNumber} has a non-boolean property hidden`,
 		}
 		this.hidden = definition.hidden;
 
+		if (
+			definition["$kind"] != undefined
+			&& typeof definition["$kind"] !== "string"
+		) {
+			throwInvalidConfig(
+				"devices",
+				`packages/config/config/devices/${parent.filename}:
+Parameter #${parameterNumber} has a non-string property $kind`,
+			);
+		}
+		this.kind = definition["$kind"];
+
 		// Parse and validate the allowed field
 		if (definition.allowed != undefined) {
 			// Validate mutual exclusivity with minValue/maxValue
@@ -368,6 +380,7 @@ Parameter #${parameterNumber}: allowed[${i}] must have either "value" or "range"
 	public readonly destructive?: boolean;
 	public readonly options: readonly ConditionalConfigOption[];
 	public readonly hidden?: boolean;
+	public readonly kind?: string;
 
 	public readonly condition?: string;
 
@@ -395,6 +408,7 @@ Parameter #${parameterNumber}: allowed[${i}] must have either "value" or "range"
 				"allowManualEntry",
 				"destructive",
 				"hidden",
+				"kind",
 			]),
 			options: evaluateDeep(this.options, deviceId, true),
 		};
--- packages/eslint-plugin/src/utils.ts
+++ packages/eslint-plugin/src/utils.ts
@@ -489,6 +489,7 @@ export const paramInfoPropertyOrder: string[] = [
 	"#",
 	"$if",
 	"$import",
+	"$kind",
 	"label",
 	"description",
 	"valueSize",
