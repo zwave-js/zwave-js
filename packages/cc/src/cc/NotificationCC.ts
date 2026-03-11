ba0a42e2baebd344daf2f95c3d7d908a6811a181
AlCalzone <dominic.griesel@nabucasa.com>
2026-03-11 23:08:42 +0100

    fix: deprecate all Door state variables, add new synthetic "Opening state" (#8676)

--- packages/cc/src/cc/NotificationCC.ts
+++ packages/cc/src/cc/NotificationCC.ts
@@ -117,8 +117,30 @@ export const NotificationCCValues = V.defineCCValues(
 				label: "Alarm Level",
 			},
 		),
+		// Synthetic notification variable to simplify working with Open/Close/Tilted states
 		...V.staticPropertyAndKeyWithName(
-			"doorStateSimple",
+			"openingState",
+			"Access Control",
+			"Opening state",
+			{
+				// Must be a number for compatibility reasons
+				...ValueMetadata.ReadOnlyUInt8,
+				label: "Opening state",
+				states: {
+					[0x00]: "Closed",
+					[0x01]: "Open",
+				},
+				ccSpecific: {
+					notificationType: 0x06,
+				},
+			},
+			{
+				autoCreate: shouldAutoCreateSyntheticDoorSensorValue,
+			},
+		),
+		// Previous attempts at fixing the door state mess, but only made things worse.
+		...V.staticPropertyAndKeyWithName(
+			"deprecated_doorStateSimple",
 			"Access Control",
 			"Door state (simple)",
 			{
@@ -134,11 +156,12 @@ export const NotificationCCValues = V.defineCCValues(
 				},
 			},
 			{
-				autoCreate: shouldAutoCreateSimpleDoorSensorValue,
+				autoCreate: shouldAutoCreateSyntheticDoorSensorValue,
 			},
 		),
+		// Previous attempts at fixing the door state mess, but only made things worse.
 		...V.staticPropertyAndKeyWithName(
-			"doorTiltState",
+			"deprecated_doorTiltState",
 			"Access Control",
 			"Door tilt state",
 			{
@@ -212,7 +235,7 @@ export const NotificationCCValues = V.defineCCValues(
 	},
 );
 
-export function shouldAutoCreateSimpleDoorSensorValue(
+export function shouldAutoCreateSyntheticDoorSensorValue(
 	ctx: GetValueDB,
 	endpoint: EndpointId,
 ): boolean {
